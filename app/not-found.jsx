428352b493bd24a7f9f4e67bb66021ffd7e019f3
arsistemadpw13 <arsistemadpw@gmail.com>
2024-08-07 14:54:48 -0400

    primer commit

---
+++ app/not-found.jsx
@@ -0,0 +1,10 @@
+const Nofound = () => {
+    return (
+      <div>
+        <h3>La ruta que intentas acceder no existe. Por favor verifica la URL.</h3>
+        <a href="/" className="btn btn-primary">Volver a la Página Principal</a>
+    </div>
+    )
+  }
+  
+  export default Nofound
